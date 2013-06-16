914aaafd2adc14bc96d9cf7c9eec79e1ade0e2d1
Mark S. Petrovic <mspetrovic@gmail.com>
2013-06-16 12:17:44 -0700

    add grammar and test harness for labelled expression example

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-A Gradle build template for ANTL4 projects.
+A Gradle build template for ANTL4 projects.  Just type "gradlew
+build" to compile the grammar files and begin using in your src/main
+and src/test source roots.
 
-Inspired by 
+This work inspired by
 
 http://fenrock.wordpress.com/2012/05/17/gradle-and-antlr-3-x/
 
@@ -9,8 +11,8 @@ Remove the
 	apply from: file('gradle/idea.gradle')
 
 if you are not using IntelliJ.  This is an IDE nice to have that
-adds the generated sources to the IDE source roots.
+adds the generated sources to the IDE source source roots.
 
-The DOT.g4 grammar and t.dot sample input were taken from Terence
+The g4 grammar and t.* sample input files were taken from Terence
 Parr's source code for the book The Definitive ANTLR4 Reference.
 
--- src/test/java/org/petrovic/ptl/AppTest.java
+++ src/test/java/org/petrovic/ptl/AppTest.java
@@ -2,21 +2,182 @@ package org.petrovic.ptl;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.antlr.v4.runtime.tree.TerminalNode;
+import org.carpediem.LabeledExprBaseVisitor;
+import org.carpediem.LabeledExprLexer;
+import org.carpediem.LabeledExprListener;
+import org.carpediem.LabeledExprParser;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 
 public class AppTest {
-    @Test
-    public void testApp() throws IOException {
-        InputStream resourceAsStream = getClass().getResourceAsStream("/t.dot");
+
+    LabeledExprLexer lexer;
+    CommonTokenStream tokenStream;
+    LabeledExprParser parser;
+    ParseTree tree;
+
+    // This method is run before each method annotated with @Test below
+    @Before
+    public void setup() throws IOException {
+        InputStream resourceAsStream = getClass().getResourceAsStream("/t.expr");
         ANTLRInputStream antlrInputStream = new ANTLRInputStream(resourceAsStream);
-        org.carpediem.DOTLexer dotLexer = new org.carpediem.DOTLexer(antlrInputStream);
-        CommonTokenStream tokenStream = new CommonTokenStream(dotLexer);
-        org.carpediem.DOTParser parser = new org.carpediem.DOTParser(tokenStream);
-        ParseTree tree = parser.graph();
+        lexer = new LabeledExprLexer(antlrInputStream);
+        tokenStream = new CommonTokenStream(lexer);
+        parser = new LabeledExprParser(tokenStream);
+        tree = parser.prog();
         System.out.println(tree.toStringTree(parser));
+    }
+
+    @Test
+    public void testListener() throws IOException {
+        ParseTreeWalker walker = new ParseTreeWalker();
+        MyLabeledExpressionListener listener = new MyLabeledExpressionListener(parser);
+        walker.walk(listener, tree);
+    }
+
+    @Test
+    public void testVisitor() throws IOException {
+        MyVisitor visitor = new MyVisitor();
+        visitor.visit(tree);
+    }
+
+    class MyLabeledExpressionListener implements LabeledExprListener {
+
+        final Parser parser;
+
+        MyLabeledExpressionListener(Parser parser) {
+            this.parser = parser;
+        }
+
+        @Override
+        public void enterId(LabeledExprParser.IdContext ctx) {
+            System.out.printf("enterId: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitId(LabeledExprParser.IdContext ctx) {
+            System.out.printf("exitId: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterAssign(LabeledExprParser.AssignContext ctx) {
+            System.out.printf("enterAssign: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitAssign(LabeledExprParser.AssignContext ctx) {
+            System.out.printf("exitAssign: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterProg(LabeledExprParser.ProgContext ctx) {
+            System.out.printf("enterProg: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitProg(LabeledExprParser.ProgContext ctx) {
+            System.out.printf("exitProg: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterBlank(LabeledExprParser.BlankContext ctx) {
+            System.out.printf("enterBlank: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitBlank(LabeledExprParser.BlankContext ctx) {
+            System.out.printf("exitBlank: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterPrintExpr(LabeledExprParser.PrintExprContext ctx) {
+            System.out.printf("enterPrintExpr: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitPrintExpr(LabeledExprParser.PrintExprContext ctx) {
+            System.out.printf("exitPrintExpr: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterInt(LabeledExprParser.IntContext ctx) {
+            System.out.printf("enterInt: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitInt(LabeledExprParser.IntContext ctx) {
+            System.out.printf("exitInt: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterAddSub(LabeledExprParser.AddSubContext ctx) {
+            System.out.printf("enterAdd: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitAddSub(LabeledExprParser.AddSubContext ctx) {
+            System.out.printf("exitAdd: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterParens(LabeledExprParser.ParensContext ctx) {
+            System.out.printf("enterParens: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitParens(LabeledExprParser.ParensContext ctx) {
+            System.out.printf("exitParens: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void enterMulDiv(LabeledExprParser.MulDivContext ctx) {
+            System.out.printf("enterMulDiv: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitMulDiv(LabeledExprParser.MulDivContext ctx) {
+            System.out.printf("exitMulDiv: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void visitTerminal(TerminalNode node) {
+            System.out.printf("visitTerminal: %s\n", node.toStringTree(parser));
+        }
+
+        @Override
+        public void visitErrorNode(ErrorNode node) {
+            System.out.printf("visitErrorNode: %s\n", node.toStringTree(parser));
+        }
+
+        @Override
+        public void enterEveryRule(ParserRuleContext ctx) {
+            System.out.printf("enterEveryRule: %s\n", ctx.toInfoString(parser));
+        }
+
+        @Override
+        public void exitEveryRule(ParserRuleContext ctx) {
+            System.out.printf("exitEveryRule: %s\n", ctx.toInfoString(parser));
+        }
+    }
+
+    class MyVisitor extends LabeledExprBaseVisitor<Integer> {
+
+        @Override
+        public Integer visitMulDiv(LabeledExprParser.MulDivContext ctx) {
+            System.out.println("hello mulDiv visit");
+            // visit or not depending on biz logic
+            // visit MulDiv children, for example
+            return visitChildren(ctx);
+        }
+
     }
 }
