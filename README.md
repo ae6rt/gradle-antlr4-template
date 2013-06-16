a4df0b770e5a484d2d8ced8c2f182cd827b65df7
Mark S. Petrovic <mspetrovic@gmail.com>
2013-06-15 17:30:52 -0700

    Add a simple unit test to print the parse tree.
    Add DOT.g and t.dot sample input.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
+A Gradle build template for ANTL4 projects.
+
 Inspired by 
 
 http://fenrock.wordpress.com/2012/05/17/gradle-and-antlr-3-x/
+
+Remove the 
+
+	apply from: file('gradle/idea.gradle')
+
+if you are not using IntelliJ.  This is an IDE nice to have that
+adds the generated sources to the IDE source roots.
+
+The DOT.g4 grammar and t.dot sample input were taken from Terence
+Parr's source code for the book The Definitive ANTLR4 Reference.
+
--- src/test/java/org/petrovic/ptl/AppTest.java
+++ src/test/java/org/petrovic/ptl/AppTest.java
@@ -1,10 +1,22 @@
 package org.petrovic.ptl;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AppTest {
     @Test
-    public void testApp() {
-
+    public void testApp() throws IOException {
+        InputStream resourceAsStream = getClass().getResourceAsStream("/t.dot");
+        ANTLRInputStream antlrInputStream = new ANTLRInputStream(resourceAsStream);
+        org.carpediem.DOTLexer dotLexer = new org.carpediem.DOTLexer(antlrInputStream);
+        CommonTokenStream tokenStream = new CommonTokenStream(dotLexer);
+        org.carpediem.DOTParser parser = new org.carpediem.DOTParser(tokenStream);
+        ParseTree tree = parser.graph();
+        System.out.println(tree.toStringTree(parser));
     }
 }
